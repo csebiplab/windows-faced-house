f1e72498824b8a5728ccac3ac3b2886c6626c1fc
oli <mohammad.oliullah25@gmail.com>
2025-09-26 17:05:34 +0600

    refactor: Section model using descriminator

--- src/models/section.model.ts
+++ src/models/section.model.ts
@@ -1,42 +1,55 @@
-import mongoose, { Model } from "mongoose";
+import mongoose, { Schema, Model, Document } from "mongoose";
 
-interface Section {
+interface SectionBase extends Document {
   sectionId: number;
   page: string;
   kind: string;
-  state?: string;
-  title?: string;
-  buttonName?: string;
-  description?: string;
-  imgUrl?: string;
   deletedAt?: Date;
   createdAt: Date;
   updatedAt: Date;
 }
 
-const SectionSchema = new mongoose.Schema(
+// Base schema
+const SectionSchema = new Schema<SectionBase>(
   {
     sectionId: { type: Number, required: true },
     page: { type: String, required: true },
     kind: { type: String, required: true },
-    state: { type: String, required: false },
-    title: { type: String, required: false },
-    buttonName: { type: String, required: false },
-    description: { type: String, required: false },
-    imgUrl: { type: String, required: false },
     deletedAt: { type: Date, default: null },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
   },
   {
     timestamps: true,
-    toObject: { virtuals: true },
     toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    discriminatorKey: "kind", // <- important
   }
 );
 
-const SectionModel: Model<Section> =
-  (mongoose.models.Section as Model<Section>) ||
-  mongoose.model<Section>("Section", SectionSchema);
+const SectionModel =
+  mongoose.models.Section ||
+  mongoose.model<SectionBase>("Section", SectionSchema);
 
-export default SectionModel;
+/**
+ * Hero Section
+ */
+interface HeroSection extends SectionBase {
+  state: string;
+  title: string;
+  buttonName: string;
+  description: string;
+  imgUrl: string;
+}
+
+const HeroSectionSchema = new Schema<HeroSection>({
+  state: { type: String, required: true },
+  title: { type: String, required: true },
+  buttonName: { type: String, required: true },
+  description: { type: String, required: true },
+  imgUrl: { type: String, required: true },
+});
+
+const HeroSectionModel: Model<HeroSection> =
+  (SectionModel.discriminators?.HeroSection as Model<HeroSection>) ||
+  SectionModel.discriminator<HeroSection>("HeroSection", HeroSectionSchema);
+
+export { SectionModel, HeroSectionModel };
