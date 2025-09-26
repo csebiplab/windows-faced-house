9f91e3c0ab81190d23e65670d05c3f50959969fb
oli <mohammad.oliullah25@gmail.com>
2025-09-27 00:11:50 +0600

    showing section dynamically

--- src/app/(dashboard)/dashboard/sections/show-sections/page.tsx
+++ src/app/(dashboard)/dashboard/sections/show-sections/page.tsx
@@ -1,12 +1,32 @@
+import { ShowSection } from "@/components/dashboard/sections/ShowSections";
 import PageHeader from "@/components/ui/PageHeader";
 
-export default function ShowSectionsPage() {
+async function getData() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/sections`, {
+    cache: "no-store",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    console.error("Failed to fetch sections");
+  }
+
+  const data: any = await res.json();
+  return data?.data;
+}
+
+export default async function ShowSectionsPage() {
+  const data: any[] = await getData();
+
   return (
     <div>
       <PageHeader
         title="Show Sections"
         href="/dashboard/sections/add-section"
       />
+      <ShowSection data={data} />
     </div>
   );
 }
