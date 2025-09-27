f3a1dfcb1bb3c943f45a66bb70b0c71fa70265c6
oli <mohammad.oliullah25@gmail.com>
2025-09-27 22:53:53 +0600

    done uppernav

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import UpperNav from "./UpperNav";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full p-x-320 bg-white shadow-sm">
+      <UpperNav />
+    </nav>
+  );
+};
+
+export default Navbar;
