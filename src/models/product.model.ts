fda307d68d140e1b473fa6db97307b565cf3ae96
oli <mohammad.oliullah25@gmail.com>
2025-10-01 22:57:57 +0600

    created product model

---
+++ src/models/product.model.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, model, Document } from "mongoose";
+
+export type ProductType =
+  | "windows"
+  | "aluminum"
+  | "cottages"
+  | "balconies"
+  | "doors";
+
+interface IProduct extends Document {
+  title?: string;
+  type?: ProductType;
+  items?: string[];
+  description?: string;
+  priceFrom?: number;
+  priceUnit?: string;
+  imageUrl?: string;
+  deletedAt?: Date | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const ProductSchema = new Schema<IProduct>(
+  {
+    title: { type: String, required: false },
+    type: {
+      type: String,
+      enum: ["windows", "aluminum", "cottages", "balconies", "doors"],
+      required: false,
+    },
+    items: { type: [String], required: false },
+    description: { type: String, required: false },
+    priceFrom: { type: Number, required: false },
+    priceUnit: { type: String, default: "₽/m²" },
+    imageUrl: { type: String, required: false },
+    deletedAt: { type: Date, default: null },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+export const Product =
+  mongoose.models.Product || model<IProduct>("Product", ProductSchema);
